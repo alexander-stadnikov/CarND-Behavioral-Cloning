cef1ed7b7d6c2f4895555fbcd19da36d4308ad17
Alexander <alexander.stadnikov@gmail.com>
2021-02-14 12:33:39 +0100

    Draft for the model: basic learning

--- model.py
+++ model.py
@@ -1,16 +1,50 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Flatten
-from keras.layers import Conv2D
+from keras.layers import Conv2D, Activation
 from keras.layers import Lambda, Cropping2D
+from keras.optimizers import Adam
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+
+from typing import List, Tuple
 
 from data_import import read_csv
+from frame import Frame
 
 
 # Read collected input data
 samples = []
-samples_sources = ['my_direct', 'my_reverse', 'udacity']
+samples_sources = ['my_direct', 'my_reverse']
+# samples_sources = ['udacity']
 for src in samples_sources:
     samples.extend(read_csv(f"./data/{src}/driving_log.csv", speed_limit=0.1))
+
+# Split all sample onto validation and test samples
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples: List[Frame], batch_size: int) -> List[np.ndarray]:
+    while True:
+        shuffle(samples)
+        for start in range(0, len(samples), batch_size):
+            end = start + batch_size
+            batch_samples = samples[start:end]
+            images = []
+            angles = []
+
+            for frame in batch_samples:
+                for i, s in [frame.center(), frame.right(), frame.left()]:
+                    images.append(i)
+                    angles.append(s)
+
+            yield np.array(images), np.array(angles) #here we do not hold the values of X_train and y_train instead we yield the values which means we hold until the generator is running
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=64)
+validation_generator = generator(validation_samples, batch_size=64)
 
 # Create the model
 def Dave2CNN() -> Sequential:
@@ -20,35 +54,36 @@ def Dave2CNN() -> Sequential:
     For more informaion, plase, read this blog post:
     https://developer.nvidia.com/blog/deep-learning-self-driving-cars/
     """
-    def _convolution(filter_size: int, kernel_size: int) -> Conv2D:
-        return Conv2D(
-            filter_size,
-            kernel_size,
-            strides=(2, 2),
-            activation='elu',
-            padding='same'
-        )
-
     model = Sequential(
-        [
+        layers= [
             Lambda(lambda x: x/255 - 0.5, input_shape=(160, 320, 3), name='Normalization'),
             Cropping2D(cropping=((60, 25), (0, 0))),
-            _convolution(24, 5),
-            _convolution(36, 5),
-            _convolution(48, 5),
-            _convolution(64, 3),
-            _convolution(64, 3),
+            Conv2D(24, 5, strides=2, activation='elu', padding='same'),
+            Conv2D(36, 5, strides=2, activation='elu', padding='same'),
+            Conv2D(48, 5, strides=2, activation='elu', padding='same'),
+            Conv2D(64, 3, activation='elu', padding='same'),
+            Conv2D(64, 3, activation='elu', padding='same'),
             Flatten(),
             Dense(100, activation='elu'),
             Dense(50, activation='elu'),
             Dense(10, activation='elu'),
             Dense(1)
-        ]
+        ],
+        name="DAVE2"
     )
 
-    model.compile(loss='mse', optimizer='adam')
-    model.summary()
-
+    model.compile(optimizer='adam', loss='mse')
     return model
 
 model = Dave2CNN()
+model.summary()
+model.fit(
+    train_generator,
+    steps_per_epoch=len(train_samples) // 64,
+    validation_data=validation_generator,
+    validation_steps=len(validation_samples) // 64,
+    epochs=5,
+    verbose=1
+)
+
+model.save('model.h5')
